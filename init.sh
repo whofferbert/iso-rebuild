12dea6035443e82a2220c8b19645e4c85bb6ba65
William Hofferbert <will.hbert@gmail.com>
2017-12-25 22:05:00 -0500

    added example init script

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# by William Hofferbert
+# example preinstall script
+
+# need i386?
+dpkg --add-architecture i386
+
+# pre-accept some EULA
+echo "wireshark-common  wireshark-common/install-setuid boolean false" | debconf-set-selections
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections
+
+# add apt repos  here
+# get google key 
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+
+# comment out all repos
+#sed -i 's/^\([ \t]*deb\)/#\1/g' /etc/apt/sources.list{,.d/*}
+
+apt-get update
+apt-get install -y google-chrome-stable
+apt-get install -y wireshark-common ttf-mscorefonts-installer
+
+echo -en ""
